f990b29454b217c157dcc66e053c82936afff375
Daniel Glassey <daniel_glassey@sil.org>
2011-12-15 09:29:13 +0700

    update for new writing systems gui something that does something, should be refined?

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mkdir -p output/Release
-MONO_PATH="./lib/common:$MONO_PATH" gmcs -out:output/Release/wsedit.exe -lib:lib/mono -r:System.Windows.Forms,PalasoUIWindowsForms wsedit/wsedit.cs wsedit/Properties/AssemblyInfo.cs
+MONO_PATH="./lib/common:$MONO_PATH" gmcs -out:output/Release/wsedit.exe -lib:lib/mono -r:System.Windows.Forms,Palaso,PalasoUIWindowsForms,Palaso.TestUtilities src/wsedit.cs src/Properties/AssemblyInfo.cs
 cp lib/common/* output/Release/
 cp lib/mono/* output/Release/
--- src/wsedit.cs
+++ src/wsedit.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
 using Palaso.UI.WindowsForms.WritingSystems;
+using Palaso.TestUtilities;
+using Palaso.WritingSystems;
+using Palaso.WritingSystems.Migration.WritingSystemsLdmlV0To1Migration;
 
 namespace wsedit
 {
+  internal class DummyWritingSystemHandler
+
+  {
+
+	  public static void onMigration(IEnumerable<LdmlVersion0MigrationStrategy.MigrationInfo> migrationInfo)
+
+	  {
+
+	  }
+
+
+
+	  public static void onLoadProblem(IEnumerable<WritingSystemRepositoryProblem> problems)
+
+	  {
+
+	  }
+
+
+
+  }
+
 	static class WSEdit
 	{
 		/// <summary>
@@ -14,7 +40,16 @@ namespace wsedit
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			WSPropertiesDialog dlg = new WSPropertiesDialog();
+			var folder = new TemporaryFolder("WS-Edit");
+			var repository = LdmlInFolderWritingSystemRepository.Initialize(folder.Path,
+		DummyWritingSystemHandler.onMigration,
+		DummyWritingSystemHandler.onLoadProblem);
+
+			var dlg = new WritingSystemSetupDialog(repository);
+
+			dlg.WritingSystemSuggestor.SuggestVoice = true;
+
+//            WSPropertiesDialog dlg = new WSPropertiesDialog();
 			dlg.Text = String.Format("{0:s} {1:s}", Application.ProductName, Application.ProductVersion);
 			Application.Run(dlg);
 		}
