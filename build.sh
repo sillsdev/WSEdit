bc26b95a7f45841f447a7ce9d5f9fea017a4bc29
Daniel Glassey <daniel_glassey@sil.org>
2011-12-15 11:43:06 +0700

    create an artifact tarball

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ mkdir -p output/Release
 MONO_PATH="./lib/common:$MONO_PATH" gmcs -out:output/Release/wsedit.exe -lib:lib/mono -r:System.Windows.Forms,Palaso,PalasoUIWindowsForms,Palaso.TestUtilities src/wsedit.cs src/Properties/AssemblyInfo.cs
 cp lib/common/* output/Release/
 cp lib/mono/* output/Release/
+cd output/Release/ && tar cfz ../wsedit.tgz *
